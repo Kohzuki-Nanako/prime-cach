3f0880ab68828d67a1eb966b167e43b5c5e0a77b
上月那奈子 <chna18018@g.nihon-u.ac.jp>
2020-08-25 09:55:50 +0900

    commited7

---
+++ build/precache-manifest.df90e768905e1ac8699560defd8e6a0e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab70890e199efe919e50398cc859a484",
+    "url": "/prime-catch/index.html"
+  },
+  {
+    "revision": "f02ab4ebe2672b5338cd",
+    "url": "/prime-catch/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "f02ab4ebe2672b5338cd",
+    "url": "/prime-catch/static/js/2.fc1a6853.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/prime-catch/static/js/2.fc1a6853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc6141f73847ee33b7f",
+    "url": "/prime-catch/static/js/main.1723c23a.chunk.js"
+  },
+  {
+    "revision": "18b217d7ba0d79d7c662",
+    "url": "/prime-catch/static/js/runtime-main.31a94d60.js"
+  },
+  {
+    "revision": "0e8bc24af6035ebdc7959a95211b6831",
+    "url": "/prime-catch/static/media/IMG_2295.0e8bc24a.PNG"
+  },
+  {
+    "revision": "7e5dd798d05081dd2a45fa9fd2312a6c",
+    "url": "/prime-catch/static/media/poster.7e5dd798.png"
+  },
+  {
+    "revision": "d44c23ed42bbedbeaad3d9ad5f7ccfdf",
+    "url": "/prime-catch/static/media/title1.d44c23ed.png"
+  },
+  {
+    "revision": "ec841d7eff19746e5e09226dbe15c1d6",
+    "url": "/prime-catch/static/media/スクリーンショット 2020-08-16 2.51.34.ec841d7e.png"
+  },
+  {
+    "revision": "cd807909dd669b89d8a994eb8decd17e",
+    "url": "/prime-catch/static/media/スクリーンショット 2020-08-16 2.52.01.cd807909.png"
+  },
+  {
+    "revision": "39fa5a583818ba3f95957f69e1e5c667",
+    "url": "/prime-catch/static/media/スクリーンショット 2020-08-16 2.53.10.39fa5a58.png"
+  },
+  {
+    "revision": "254243c89f18de7a04e6dd935beb369f",
+    "url": "/prime-catch/static/media/スクリーンショット 2020-08-16 2.54.04.254243c8.png"
+  },
+  {
+    "revision": "6933cb092318b6a0d5659507dc129c4d",
+    "url": "/prime-catch/static/media/スクリーンショット 2020-08-21 1.14.30.6933cb09.png"
+  },
+  {
+    "revision": "1364a3fdc62ad518751444d9a79d531f",
+    "url": "/prime-catch/static/media/スクリーンショット 2020-08-21 12.57.24.1364a3fd.png"
+  },
+  {
+    "revision": "87a7d4016243dedaa458c3dae461fad4",
+    "url": "/prime-catch/static/media/スクリーンショット 2020-08-21 12.58.22.87a7d401.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/prime-catch/precache-manifest.6d120712a9242889f5c6e7173dd85bc9.js"
+  "/prime-catch/precache-manifest.df90e768905e1ac8699560defd8e6a0e.js"
 );
 
 self.addEventListener('message', (event) => {
